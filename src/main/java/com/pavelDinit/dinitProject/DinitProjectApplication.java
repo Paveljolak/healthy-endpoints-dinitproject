a6b4d1493b42df7f3bd1d5da64412fe14367bf86
Paveljolak <paveljolak123@gmail.com>
2024-07-01 14:55:41 +0200

    Initial set-up

---
+++ src/main/java/com/pavelDinit/dinitProject/DinitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pavelDinit.dinitProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavelDinit/dinitProject/DinitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavelDinit.dinitProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
